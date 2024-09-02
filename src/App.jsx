b2a7876b16db6527ecebf0dc25656060996c96d3
Your Name <sakshyambanjade@gmail.com>
2024-09-02 23:04:35 +0545

    basic theme done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Sidebar from './components/sidebar'
+import Player from './components/Player'
 const App = () => {
   return (
     <div className='h-screen bg-black'>
       <div className="has-[90%] flex">
         <Sidebar/>
+      </div>
+      <div>
+     <Player/>
       </div>
     </div>
   )
